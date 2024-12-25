d4e84455acbfed5a7bd5c8eb19fdbb1c08f2c38c
Priyanshu Singh <priyanshu40507@gmail.com>
2024-12-25 10:29:03 +0530

    add search bar to header component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
+import { Search } from 'lucide-react';
+
+
 export default function Header() {
     return (
-    <div>
-        {/* Left */}
+    
+        
         <div className="flex items-center justify-between max max-w-6xl">
+            {/* Left */}
             <div className="cursor pointer = h-24 w-24 relative hidden lg:inline-grid">
                 <Image
                 src="https://logos-world.net/wp-content/uploads/2020/04/Instagram-Logo.png"
@@ -21,10 +25,17 @@ export default function Header() {
                 className='object-contain'
                 />
             </div>
-            <h1>Right side</h1>
-        </div>
         {/* Middle */}
+
+        <div className="relative m-1">
+            <div className="absolute top-2 left-2">
+                <Search className="h-5" text-gray-500/>
+            </div>
+            <input type="text" placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:border-black rounded-md'/>
+        </div>
         {/* Right */}
+            <h1>Right side</h1>
+        
     </div>
     )
 }
